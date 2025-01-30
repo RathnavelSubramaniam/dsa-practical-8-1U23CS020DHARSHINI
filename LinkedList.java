f5eb43a714511396ceaf97b5a5dec6d34e465e3a
1U23CS020DHARSHINI <159122276+1U23CS020DHARSHINI@users.noreply.github.com>
2025-01-30 16:22:44 +0000

    submit8

--- LinkedList.java
+++ LinkedList.java
@@ -61,17 +61,28 @@ int data,data1;
           }
         }
     }
-      public void insertEnd(int data) 
-    {    
-       //TYPE YOUR CODE HERE 
-    }  
-     
-public void displayList() {
-        Node current = head;
-        while (current != null) {
-System.out.print(current.data + " -> ");
-            current = current.next;
-        }
-System.out.println("null");
-    }
+    
+    public void insertEnd(int data) 
+        {    
+               
+                      Node newNode = new Node(data);
+
+                        
+                              if (head == null) {
+                                      head = newNode;
+                                              tail = newNode;
+                                                  } else {
+                                                          tail.next = newNode;
+                                                                  tail = newNode;   
+                                                                      }
+                                                                          }  
+
+                                                                          public void displayList() {
+                                                                                  Node current = head;
+                                                                                          while (current != null) {
+                                                                                          System.out.print(current.data + " -> ");
+                                                                                                      current = current.next;
+                                                                                                              }
+                                                                                                              System.out.println("null");
+                                                                                                                  }
 }
